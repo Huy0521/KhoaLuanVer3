c5602b991b683d515af151e8378ea4a581f22216
Huy0521 <you@example.com>
2023-03-15 16:18:48 +0700

    4

--- Main/Assets/SpawPlayer.cs
+++ Main/Assets/SpawPlayer.cs
@@ -13,10 +13,8 @@ public class SpawPlayer : MonoBehaviour
     {
         Vector2 randomPosition = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
         GameObject pl =  PhotonNetwork.Instantiate(playerPrefab.name, randomPosition, Quaternion.identity);
-        Debug.Log(pl.GetComponent<PhotonView>().IsMine);
         if(pl.GetComponent<PhotonView>().IsMine)
         {
-            Debug.Log("trueeee");
             PopupManager.Instance.playerControllerInArena = pl.GetComponent<PlayerControllerInArena>();
         }
         
